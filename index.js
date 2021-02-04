af18196dc8a61f1537f68c8d5cb817a196e50d68
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2021-02-04 15:45:22 +0100

    rewritten

--- index.js
+++ index.js
@@ -1,47 +1,11 @@
-const faders = document.querySelectorAll(".fade-in");
-const options = {
-    threshold: 1,
-    rootMargin: "0px 0px -5px 0px"
-};
-const AppearOnScroll = new IntersectionObserver(function(entries,AppearOnScroll) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add("appear");
-            AppearOnScroll.unobserve(entry.target);
-        }
-    })
-}, options);
-
-faders.forEach(fader => {
-    AppearOnScroll.observe(fader);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function textChange(){
-    document.getElementById('section-title_button-IP').innerHTML = "--";
+function responsivenavbar() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
   }
-  
-  function ClickFuncsFeedback(){
-      textChange();
-  }
-
-
-
-  function ServerInvite() {
+function DiscordInvite() {
     window.open("https://discord.gg/F2zFuaB")
   }
