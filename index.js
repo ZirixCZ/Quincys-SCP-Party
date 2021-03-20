19a738b30fdaed3c77afc0fa48f1a9649b113639
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2021-03-20 22:25:27 +0100

    navbar transition

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function HomeOpen() {
     window.open("https://www.quincyscp.ga/")
   }
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > 900) {
+        $(".navbar").css("background" , "rgb(9, 9, 9)");
+      }
+   
+      else{
+        $(".navbar").css("background" , "transparent");  
+      }
+    })
+  })
