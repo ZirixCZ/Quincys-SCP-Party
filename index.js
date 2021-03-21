0d925d7a1fc0e86ce98877d514c2aac617877f3a
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2021-03-21 18:17:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 function HomeOpen() {
     window.open("https://www.quincyscp.ga/")
   }
-  $(document).ready(function(){
-    $(window).scroll(function(){
-      var scroll = $(window).scrollTop();
-      if (scroll > 900) {
-        $(".navbar").css("background" , "rgb(9, 9, 9)");
-      }
-   
-      else{
-        $(".navbar").css("background" , "transparent");  
-      }
-    })
-  })
+
+  window.addEventListener('scroll', event => document.querySelector('.navbar').classList [600 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('dark')
+  );
