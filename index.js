2ec9f074d28b2c52dd85433f4598f85625249eb3
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2021-03-22 18:11:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function HomeOpen() {
   window.addEventListener('scroll', event => document.querySelector('.navbar').classList [600 < document.scrollingElement.scrollTop ? 'add' : 'remove']
   ('dark')
   );
+  window.addEventListener('scroll', event => document.querySelector('.parallax').classList [200 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('shader')
+  );
